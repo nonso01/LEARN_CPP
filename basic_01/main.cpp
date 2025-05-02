b9abc3eb9cad8ba7e0e5aaf376211f05f63323ad
nonso01 <chukwunonsomartin01@gmail.com>
2025-05-02 11:02:46 +0100

    init

---
+++ basic_01/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int add, sub, mul, div, mod;
+    add = 4 + 5;
+    sub = 4 - 5;
+    mul = 4 * 5;
+    div = 4 / 5;
+    mod = 4 % 5;
+    
+    cout << "Addition: " << add << endl;
+    cout << "Subtraction: " << sub << endl;
+    cout << "Multiplication: " << mul << endl;
+    cout << "Division: " << div << endl;
+    cout << "Modulus: " << mod << endl;
+
+    // cout << add << endl; 
+    return 0;
+}
