b3c90aa0cda4999dff26ad84e6bae36f31cff268
nonso01 <chukwunonsomartin01@gmail.com>
2025-05-04 17:02:01 +0100

    for loops

--- basic_01/main.cpp
+++ basic_01/main.cpp
@@ -4,19 +4,20 @@ using namespace std;
 
 int main()
 {
-    int add, sub, mul, div, mod;
-    add = 4 + 5;
-    sub = 4 - 5;
-    mul = 4 * 5;
-    div = 4 / 5;
-    mod = 4 % 5;
-    
-    cout << "Addition: " << add << endl;
-    cout << "Subtraction: " << sub << endl;
-    cout << "Multiplication: " << mul << endl;
-    cout << "Division: " << div << endl;
-    cout << "Modulus: " << mod << endl;
+    int loop(int);
 
-    // cout << add << endl; 
+    loop(12);
+    //    cout << add << endl;
+    return 0;
+}
+
+int loop(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        if (i == 11)
+            break;
+        cout << i << endl;
+    }
     return 0;
 }
